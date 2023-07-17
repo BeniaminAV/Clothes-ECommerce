d9040c65f4c939f88966c9bdf51e0b4e74b91307
BeniaminAV <57075208+BeniaminAV@users.noreply.github.com>
2023-07-18 00:36:28 +0300

    Update cart.context.jsx

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -13,7 +13,27 @@ const addToCart = (cartItems, addToProduct) => {
     )
   }
 
-  return [...cartItems, { ...productToAdd, quantity: 1 }]
+  return [...cartItems, { ...addToProduct, quantity: 1 }]
+}
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  )
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id)
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  )
+}
+
+const clearCartItem = (cartItems, cartItemToClear) => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id)
 }
 
 export const CartContext = createContext({
